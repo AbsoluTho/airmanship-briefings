a2ee791596e2a69f35dde451e55d25cfc2fcc2a1
AbsoluTho <jonathan.tho@gmail.com>
2026-09-23 00:24:45 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Airmanship Briefings service worker. Change CACHE_VERSION on every release so devices drop the old files. */
-const CACHE_VERSION='1.15.1';
+const CACHE_VERSION='1.16.0';
 const CACHE='airmanship-briefings-'+CACHE_VERSION;
 const ASSETS=[
   "./",
